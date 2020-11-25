af5518eacf16dd2967b808019e0d55c35b07e49b
Boris Golynov <o1neill@yandex.ru>
2020-11-25 14:49:58 +0300

    CleenCodeAirportVersion1

---
+++ CleenCode/MainTestAirport/Java/src/main/java/Planes/extendsFromPlane.java
@@ -0,0 +1,7 @@
+package Planes;
+
+class extendsFromPlane extends Plane {
+    public extendsFromPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+}
